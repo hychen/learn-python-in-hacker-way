ed688a3bffcf831d1038e1957736c4d1cd065fd6
Samantha Jian-Pielak <samantha.jian-pielak@canonical.com>
2012-04-30 00:56:11 -0400

    week2 assignment

--- courses/novice/exercises/week2/5.py
+++ courses/novice/exercises/week2/5.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-# 將 data1 與 data2 合併成為一個陣列, 合併規則為
-# [ (data1 第一個元素, data2 第一個元素), (data1 第二個元素, data2 第二個元素), .... ]
+
 answer = None
 data1 = [1,2,3,4,5]
 data2 = ['A', 'B', 'C', 'D', 'E']
+
+answer = zip(data1, data2)
+
 assert answer == [ (1,'A'), (2,'B'), (3,'C'), (4, 'D'),(5,'E') ]
